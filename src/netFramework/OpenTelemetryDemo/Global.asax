f85fe0286a49d9cef266527f8e42e590aa8ac036
Jens Melgaard <admin@it-links.dk>
2022-10-14 09:20:31 +0200

    Spike

---
+++ src/netFramework/OpenTelemetryDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OpenTelemetryDemo.WebApiApplication" Language="C#" %>
---
+++ src/netFramework/OpenTelemetryDemo/Global.asax.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace OpenTelemetryDemo
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configuration.Formatters.Remove(
+                GlobalConfiguration.Configuration.Formatters.XmlFormatter);
+            GlobalConfiguration.Configure(Register);
+        }
+
+        public static void Register(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+        }
+    }
+}
