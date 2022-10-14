daf1f6cfce71621b39e0028fc351bba2e0fab8fa
Jens Melgaard <admin@it-links.dk>
2022-10-14 09:40:34 +0200

    OTLP Configuration

--- src/net6/OpenTelemetryDemo/Program.cs
+++ src/net6/OpenTelemetryDemo/Program.cs
@@ -1,4 +1,29 @@
+using OpenTelemetry;
+using OpenTelemetry.Exporter;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+string serviceName = "Systematic.Demo.Service";
+string serviceVersion = "1.0.0";
+
+builder.Services.AddOpenTelemetryTracing(builder => {
+    builder
+        .AddSource(serviceName)
+        .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(serviceName, serviceVersion))
+        .AddHttpClientInstrumentation()
+        .AddAspNetCoreInstrumentation()
+        .AddSqlClientInstrumentation()
+        .AddOtlpExporter(options => {
+            options.ExportProcessorType = ExportProcessorType.Simple;
+            options.Endpoint = new Uri("http://sl-clud-048:4317");
+            options.Protocol = OtlpExportProtocol.Grpc;
+        });
+});
+
+
 WebApplication app = builder.Build();
 app.MapGet("/api/values", () =>
 {
--- src/netFramework/OpenTelemetryDemo/Global.asax.cs
+++ src/netFramework/OpenTelemetryDemo/Global.asax.cs
@@ -6,6 +6,10 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using OpenTelemetry;
+using OpenTelemetry.Exporter;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 
 namespace OpenTelemetryDemo
 {
@@ -15,12 +19,48 @@ namespace OpenTelemetryDemo
         {
             GlobalConfiguration.Configuration.Formatters.Remove(
                 GlobalConfiguration.Configuration.Formatters.XmlFormatter);
+            
+            Telemetry.Initialize();
             GlobalConfiguration.Configure(Register);
+
+
         }
 
         public static void Register(HttpConfiguration config)
         {
             config.MapHttpAttributeRoutes();
+        }
+
+        protected void Application_End()
+        {
+            Telemetry.Dispose();
+        }
+    }
+
+    public static class Telemetry 
+    {
+        private const string serviceName = "Systematic.Demo.Service";
+        private const string serviceVersion = "1.0.0";
+
+        private static TracerProvider tracerProvider;
+
+        public static void Initialize()
+        {
+            tracerProvider = Sdk.CreateTracerProviderBuilder()
+                .AddAspNetInstrumentation()
+                .AddSource(serviceName)
+                .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(serviceName, serviceVersion))
+                .AddOtlpExporter(options => {
+                    options.ExportProcessorType = ExportProcessorType.Simple;
+                    options.Endpoint = new Uri("http://sl-clud-048:4317");
+                    options.Protocol = OtlpExportProtocol.Grpc;
+                })
+                .Build();
+        }
+
+        public static void Dispose()
+        {
+            tracerProvider?.Dispose();
         }
     }
 }
